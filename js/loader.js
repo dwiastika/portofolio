31e3f08b7282cbf3591dd04d088dda85451d6fcb
dwiastika <dwi9astika@gmail.com>
2026-02-22 13:44:19 +0700

    Update

---
+++ js/loader.js
@@ -0,0 +1,11 @@
+async function loadComponent(id, file) {
+    const res = await fetch(file);
+    const data = await res.text();
+    document.getElementById(id).innerHTML = data;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadComponent("navbar", "./components/navbar.html");
+    loadComponent("footer-global", "./components/footer.html");
+    loadComponent("whatsapp-global", "./components/whatsapp.html");
+});
